b240dcb931b8835277a44ed304458ffa4c24e639
Francisco Hauva <phauva@proton.me>
2024-07-22 01:48:32 -0400

    feat: better text overflow wrapping

--- src/renderer/src/components/ChatEvent.svelte
+++ src/renderer/src/components/ChatEvent.svelte
@@ -12,7 +12,7 @@
 <!-- TODO: implement logic and design for all channel events -->
 {#if event.action === "message"}
 	<li class="flex gap-2">
-		<div>
+		<p class="wrap">
 			<span class="text-zinc-300"
 				>[{getFormattedTimestamp(event.timestamp)}]
 			</span>
@@ -21,7 +21,13 @@
 					href="https://osu.ppy.sh/users/{event.username}"
 					target="_blank">{event.username}</a
 				>: {" "}
-			</span><span class="break-all text-white">{event.message}</span>
-		</div>
+			</span><span class="text-white">{event.message}</span>
+		</p>
 	</li>
 {/if}
+
+<style>
+	.wrap {
+		overflow-wrap: anywhere;
+	}
+</style>
