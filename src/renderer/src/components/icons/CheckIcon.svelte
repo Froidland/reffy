557eead19951aeef90e94e369eb2f596a574ec29
Francisco Hauva <phauva@proton.me>
2024-11-11 00:08:03 -0300

    feat: new chekbox component and bump dependencies

---
+++ src/renderer/src/components/icons/CheckIcon.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { SVGAttributes } from "svelte/elements";
+
+	let {
+		width = "1em",
+		height = "1em",
+		viewBox = "0 0 24 24",
+		...props
+	}: SVGAttributes<SVGElement> = $props();
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" {width} {height} {viewBox} {...props}
+	><path
+		fill="currentColor"
+		d="m9.55 18l-5.7-5.7l1.425-1.425L9.55 15.15l9.175-9.175L20.15 7.4z"
+	/></svg
+>
---
+++ src/renderer/src/components/ui/Checkbox.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { Checkbox, Label } from "bits-ui";
+	import CheckIcon from "../icons/CheckIcon.svelte";
+	import { generateId } from "../../utils";
+
+	type Props = {
+		label: string;
+		value?: boolean;
+		name?: string;
+	};
+
+	let { label, value = $bindable(false), name }: Props = $props();
+
+	const checkboxId = generateId();
+	const labelId = generateId();
+</script>
+
+<div class="flex gap-2">
+	<Checkbox.Root
+		bind:checked={value}
+		id={checkboxId}
+		aria-labelledby={labelId}
+		{name}
+	>
+		<Checkbox.Indicator class="rounded border p-[1px] text-white">
+			{#if value}
+				<CheckIcon />
+			{:else}
+				<div class="h-[1em] w-[1em]"></div>
+			{/if}
+		</Checkbox.Indicator>
+	</Checkbox.Root>
+	<Label.Root class="text-white" id={labelId} for={checkboxId}
+		>{label}</Label.Root
+	>
+</div>
